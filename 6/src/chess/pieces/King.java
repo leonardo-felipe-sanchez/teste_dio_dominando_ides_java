741a928d6e7ec9e8d14c8c34f29b20067c225b0a
leonardo-felipe-sanchez <felipinhosanchez77@gmail.com>
2022-06-17 17:00:19 -0300

    uuuuuui

---
+++ 6/src/chess/pieces/King.java
@@ -0,0 +1,17 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.Color;
+
+public class King {
+
+	public King(Board board, Color color) {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "K";
+	}
+	
+}
