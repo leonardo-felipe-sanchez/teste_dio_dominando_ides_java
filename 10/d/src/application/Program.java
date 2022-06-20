59e8a081026c225f1899a66183012df73aab5bee
leonardo-felipe-sanchez <felipinhosanchez77@gmail.com>
2022-06-20 18:32:12 -0300

    shopee

---
+++ 10/d/src/application/Program.java
@@ -0,0 +1,38 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.function.Predicate;
+
+import entities.Product;
+import util.ProductPredicate;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
+		
+		double min = 100.0;
+		
+//		Predicate<Product> pred = p -> p.getPrice() >= min;
+		
+		list.removeIf(/*p -> p.getPrice() >=100*/
+						/*new ProductPredicate()*/
+				/*Product::nonstaticProductPredicate*/
+				/*pred*/
+				p -> p.getPrice() >=min	);
+		
+		for(Product p : list) {
+			System.out.println(p);
+		}
+	}
+
+}
